8763de652cc173e67cd815ebba5e792b161abf4f
Deirdre McCarthy <deirdremc1968@gmail.com>
2024-01-09 20:05:56 +0000

    Attempt to load background image

--- reloved/settings.py
+++ reloved/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-MEDIAL_URL = '/media/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
